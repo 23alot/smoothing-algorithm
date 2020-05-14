d4180fc59c12cd5eea42147b3b28f6e8d4bf1a86
23alot <ayumamontov@gmail.com>
2020-05-14 10:05:53 +0300

    Перенес алгоритм в kotlin library

--- src/main/kotlin/com.supesuba.smoothing/domain/PNTriangleSmoothingInteractor.kt
+++ src/main/kotlin/com.supesuba.smoothing/domain/PNTriangleSmoothingInteractor.kt
@@ -3,15 +3,17 @@ package com.supesuba.smoothing.domain
 import com.supesuba.smoothing.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flow
+
+//import kotlinx.coroutines.flow.MutableStateFlow
 
 class PNTriangleSmoothingInteractor(
 
 ): SmoothingInteractor {
     private val triangles = mutableListOf<PNTriangle>()
 
-    @ExperimentalCoroutinesApi
-    private val state = MutableStateFlow<RenderObject>(RenderObject())
+//    @ExperimentalCoroutinesApi
+//    private val state = MutableStateFlow<RenderObject>(RenderObject())
 
 
     override suspend fun addTriangle(triangle: Triangle) {
@@ -22,18 +24,18 @@ class PNTriangleSmoothingInteractor(
     override suspend fun tessellate(tessellationLevel: Int) {
         val newTriangles = triangles.map { it.tessellate(tessellationLevel) }.flatten()
         val vertices = newTriangles.map { it.toList() }.flatten()
-        state.value = RenderObject(
-            verticesArray = vertices.map { it.toList() }.flatten().toFloatArray(),
-            normalsArray = vertices.map { it.getNormalList() }.flatten().toFloatArray()
-        )
+//        state.value = RenderObject(
+//            verticesArray = vertices.map { it.toList() }.flatten().toFloatArray(),
+//            normalsArray = vertices.map { it.getNormalList() }.flatten().toFloatArray()
+//        )
     }
 
     override suspend fun calculateVertexNormals(vertices: List<Vertex>) {
-        vertices.forEachIndexed { index, vertex ->
+        vertices.forEachIndexed { _, vertex ->
             vertex.toVertexNormal(triangles.map(PNTriangle::triangle).filter { vertex in it.toList() })
         }
     }
 
     @ExperimentalCoroutinesApi
-    override suspend fun observeModelChanges(): Flow<RenderObject> = state
+    override suspend fun observeModelChanges(): Flow<RenderObject> = flow {  }
 }
